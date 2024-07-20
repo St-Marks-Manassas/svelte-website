aecc9634f7f0cda5facd03561362be9366d769ab
Jason Hall <jdhall75@gmail.com>
2024-07-20 19:58:27 -0400

    Semi sortof got a layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,70 @@
 <script>
-	import Header from './Header.svelte';
 	import '../app.css';
+	import { page } from '$app/stores';
+	import {
+		DarkMode,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Button,
+		Heading,
+		P,
+	} from 'flowbite-svelte';
+		import { Cog } from 'svelte-heros-v2';
+
+	let backdrop = false;
+	const toggleDrawer = () => {
+		drawerHidden = false;
+	};
+	$: activeUrl = $page.url.pathname;
+	let spanClass = 'pl-2 self-center text-md text-gray-900 whitespace-nowrap dark:text-white';
+	let divClass = 'w-full ml-auto lg:block lg:w-auto order-1 lg:order-none';
+	let ulClass =
+		'flex flex-col py-3 my-4 lg:flex-row lg:my-0 text-sm font-medium gap-4 dark:lg:bg-transparent lg:bg-white lg:border-0';
+	let navDivClass =
+		'bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 flex items-center justify-between w-full mx-auto py-1.5 px-4';
 </script>
 
-<div class="app">
-	<Header />
 
-	<main>
+<svelte:head>
+    <script src="https://www.youtube.com/iframe_api"></script>
+</svelte:head>
+
+
+<header class="flex-none w-full mx-auto bg-white dark:bg-slate-950">
+	<Navbar let:hidden let:toggle>
+		<NavBrand href="/" >
+			<Cog />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white pl-4">
+				St Marks UMC
+			</span>
+		</NavBrand>
+ 	   <div class="flex md:order-2">
+ 		<Button size="sm">Prayer Request</Button>
+ 		<NavHamburger />
+	  </div>
+		<NavUl>
+			<NavLi class="lg:px-2 lg:mb-0" active={activeUrl === '/'} href="/">Home</NavLi>
+			<NavLi class="lg:px-2 lg:mb-0" active={activeUrl === '/pages/about'} href="/pages/about">About</NavLi>
+			<NavLi class="lg:px-2 lg:mb-0" href="https://github.com/shinokada/flowbite-sveltekit-responsive-sidebar-layout">GitHub</NavLi>
+		</NavUl>
+	</Navbar>
+</header>
+<div class="flex px-4 mx-auto w-full">
+	<main class="w-full mx-auto">
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
+<footer class="flex-none mx-auto bg-white dark:bg-slate-950">
+	<div class="grid xl:grid-cols-2 sm:grid-cols-1 gap-4 mx-auto w-4/5 place-items-start">
+		<div>
+			<Heading tag="h4">Location</Heading>
+		</div>
+		<div>
+			<Heading tag="h4">Site</Heading>
+		</div>
+	</div>
+</footer>
 
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,67 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import { Heading } from "flowbite-svelte";
+  import Youtube from "$lib/components/YouTube.svelte";
+
+  let player;
+
+  const toggle = () => {
+    console.log('changing video id')
+    player.loadVideoById("dQw4w9WgXcQ");
+  }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Join Us</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Senectus et netus
+            et malesuada fames ac turpis egestas sed. Arcu non sodales neque
+            sodales ut. Etiam non quam lacus suspendisse. Tortor id aliquet lectus
+            proin nibh nisl.
+            </p>
+            <a href="#" class="bg-red-200 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-red-300 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Contact Us
+            </a> 
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex bg-white-800 bg-cover bg-center bg-[url('https://placehold.co/600x400?text=Image+of+church')] bg-blend-lighten h-600 w-400 rounded-md" >
+        </div>
+    </div>
 </section>
+<section class="bg-red-50 rounded-md dark:bg-gray-900 py-4">
+    <!-- events container -->
+    <div class="grid max-w-screen-xl mx-auto px-4 lg:gap-4 lg:py-8 lg:grid-cols-12 bg-white rounded">
+        <div class="place-self-start h-full w-full lg:col-span-6 bg-white px-8 py-8 rounded">
+            <h3 class="max-w-2xl mb-4 text-4xl font-extrabold text-gray-800 tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+              Worship
+            </h3>
+            <p class="max-w-2xl mb-6 font-light text-gray-700 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+           
+            <p class="text-xl">In person worship at 11:00 AM</p>
+            <p class="text-xl">Youth and Adult Sunday school start at 9:45 AM</p>
+           
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+        </div>
+        <div class="place-self-center h-full w-full lg:col-span-6 bg-white px-8 py-8 rounded">
+            <div><p class="text-2xl font-black text-center">Recent Sermon</p></div>
+        <div>
+        <Youtube id="nmcXGW-d8Fw" />
+        </div>
+        </div>
+    </div>
+</section>
+<section class="bg-white rounded-md dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="place-self-start lg:col-span-7">
+            <h3 class="font-serif max-w-2xl mb-4 text-4xl font-extrabold text-gray-800 tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+              Events
+            </h3>
+            <p class="max-w-2xl mb-6 font-light text-gray-700 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+    </div>
+</section>
